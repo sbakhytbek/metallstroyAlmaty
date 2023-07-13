dcdcd662ab730657c25190d6d401fea9ea2f5db9
Sanzhikkk <bsbakhytbeksanzhar@gmail.com>
2023-07-13 11:54:38 +0600

    Initial Commit

---
+++ metallstroyAlmaty/metallstroyAlmatyApp.swift
@@ -0,0 +1,17 @@
+//
+//  metallstroyAlmatyApp.swift
+//  metallstroyAlmaty
+//
+//  Created by Санжар Бахытбек on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct metallstroyAlmatyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
